27950a2e664f9d64c6e015f04c6b9d32f7055e60
Boobeh123 <boobeh321@gmail.com>
2022-05-21 16:36:41 -1000

    Add while loop to construct object & for loop to animate object

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ Get access to drawing properties and methods
 const canvas = document.querySelector('canvas');
 // Drawing context on the canvas. Supports 2D and 3D. "Tells computer to use the 2d library"
 const ctx = canvas.getContext('2d');
-// Set width & height on the canvas to the innerWidth of the window.
+// Set width & height of the canvas to the viewport .
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 // This function generates random (whole) numbers.
 // The numbers generated will control ball size, velocity, and inertia.
-function randomNumberGenerator() {
-    return Math.floor(Math.random() * 255);
+function randomNumberGenerator(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // This function generates a random RGB color
 // The numbers generated picks a RGB color between 0 and 255
 function randomColorGenerator() {
-    return `rgb(${randomNumberGenerator}, ${randomNumberGenerator}, ${randomNumberGenerator})`
+    return `rgb(${randomNumberGenerator(0, 255)}, ${randomNumberGenerator(0, 255)}, ${randomNumberGenerator(0, 255)})`   
 }
 
 // Creates ball objects
@@ -85,13 +85,42 @@ class Ball {
         this.y += this.velocityY;
     }
 }
+// Balls that will load on page load
+const balls = [];
+// A while loop that creates Ball objects (with random properties)
+while (balls.length <= 10) {
+    // Creates Ball objects with the Ball constructor
+    const size = randomNumberGenerator(1, 20)
+    const ball = new Ball(
+        // x coordinate in Ball constructor parameter
+        randomNumberGenerator(0 + size, width - size),  // Min = Left side of screen and buffer ball size && Max = Right side of the screen and buffer ball size
+        // y coordinate in Ball constructor parameter
+        randomNumberGenerator(0 + size, height - size), // Min = Bottom side of screen and buffer ball size && Max = Top side of the screen and buffer ball size
+        // horizontal velocity in Ball constructor parameter
+        randomNumberGenerator(1, 4),
+        // vertical velocity in Ball constructor parameter
+        randomNumberGenerator(1, 4),
+        // color in Ball constructor parameter
+        randomColorGenerator(),
+        // size in Ball constructor parameter
+        size
+    )
+    
+    // Adds 10 ball objects into the array.
+    balls.push(ball);
+}
 
-
-
-// 2nd rng 
-// function randomNumberGenerator2(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// function randomColorGenerator2() {
-//     return `rgb(${randomNumberGenerator2(0, 255)}, ${randomNumberGenerator2(0, 255)}, ${randomNumberGenerator2(0, 255)})`
-// }
+// A for loop that calls the draw method and the update method
+// Draw draws the object. Update updates the position of the object.
+function loopAnimations() {
+    // For of loop to loop the draw & update method
+    for (const ball of balls) {
+        ball.draw()
+        ball.update()
+    }
+    // Built-in function that draws a frame of animation
+    // This (recursive) function calls the loopAnimations function
+    requestAnimationFrame(loopAnimations);
+}
+// Calls the for loop on page load. The recursive function will keep calling after.
+loopAnimations();
